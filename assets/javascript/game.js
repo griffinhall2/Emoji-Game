c80234b2ea795f88022c218219800147efc13700
griffinhall2 <griffin.hall@yahoo.com>
2018-07-24 06:26:31 -0400

    additions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,13 @@
-// zero out the variables
+//default the variables
 var wins = 0;
 var losses = 0;
-var computerScore = 0;
+var computerScore = Math.floor(Math.random() * 102) + 19;
 var userScore = 0;
-
-function () {
+var bear = Math.floor(Math.random() * 12) + 1;
+var dog = Math.floor(Math.random() * 12) + 1;
+var horse = Math.floor(Math.random() * 12) + 1;
+var pig = Math.floor(Math.random() * 12) + 1;
+console.log(pig);
+console.log(bear);
+console.log(dog);
+console.log(horse);
